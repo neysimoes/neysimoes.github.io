81dfdb57de33a7e6d4eca3c31e0e9ac9b0efc439
neysimoes <neysimoes@gmail.com>
2014-07-30 18:04:19 -0300

    New website

---
+++ gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+	'use strict';
+	grunt.initConfig({
+		connect: {
+			server: {
+				options: {
+					protocol: 'http',
+					// port: 8080,
+					keepalive: true,
+					livereload: true
+				}
+			}
+		},
+		clean: {
+			dist: ['assets/css/'],
+		},
+		stylus: {
+			compile: {
+				options: {
+					paths: ['assets/css/', 'assets/styl/'],
+					compress: false,
+					// banner: '/* VoraxLab 2014 */'
+				},
+				files: {
+					'assets/css/main.css': ['assets/styl/main.styl']
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['assets/styl/*.styl'],
+				tasks: ['clean', 'stylus'],
+				options: {
+					livereload: true
+				}
+			}
+		}
+	});
+
+	// load tasks
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+	grunt.registerTask('w', ['watch']);
+	grunt.registerTask('build', ['clean', 'stylus']);
+};
